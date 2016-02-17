9301f2eb0777ae7b0156e66ed842aea57ffd2241
SebastianWaldau <sebastian.waldau@gmail.com>
2016-02-17 18:08:50 +0100

    Revert "Bump"
    This reverts commit 0496718b44466a20908fe3318e6a1b97ab50c80d.

--- src/game/Engine/AbstractGame.java
+++
@@ -1,6 +0,0 @@
-package game.Engine;
-
-public abstract class AbstractGame {
-	public abstract void update(FrameWork fw, float dt);
-	public abstract void render(FrameWork fw, Renderer r);
-}
--- src/game/Engine/InputState.java
+++
@@ -1,9 +0,0 @@
-package game.Engine;
-
-/*all different states of a key*/
-public enum InputState {
-	StillReleased,
-	JustPressed,
-	JustReleased,
-	StillPressed
-}
